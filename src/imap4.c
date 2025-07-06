36fdb060a0e73a1baf930d98551aadc464923053
Tom Ivar Helbekkmo <tih@hamartun.priv.no>
2025-07-06 20:11:37 +0200

    undo last

--- src/imap4.c
+++ src/imap4.c
@@ -162,8 +162,6 @@ void imap_cleanup_deferred(gpointer data)
 	ci_close(ci);
 	ci = NULL;
 
-	TRACE(TRACE_NOTICE, "[%p] session closed", session);
-
 	dbmail_imap_session_delete(&session);
 
 	if (rx == STDIN_FILENO)
